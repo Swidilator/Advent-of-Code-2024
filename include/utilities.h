39653f2a1e044ae081b08fbb9a98772abb2f2c46
Swidilator <5881366+swidilator@users.noreply.github.com>
2024-12-08 17:23:38 +0200

    Update utilities.h.

--- include/utilities.h
+++ include/utilities.h
@@ -12,10 +12,43 @@
 namespace util {
     using std::operator""sv;
 
+    // template<typename R, typename T>
+    // concept input_range_of =
+    //     std::ranges::input_range<R> &&
+    //         std::convertible_to<std::ranges::range_value_t<R>, T>;
+    //
+    //
+    // class VectorStringToInt {
+    // public:
+    //     std::vector<int> data{};
+    //     explicit VectorStringToInt(input_range_of<int> auto&& range)
+    //     {
+    //         data = range | std::views::transform([](const auto&& inp) {
+    //             return std::stoi(inp);
+    //         }) | std::ranges::to<std::vector>();
+    //     }
+    // };
+
+    // inline int string_to_int(const std::string_view &input) {
+    //     return string_to_int(std::string(input));
+    // }
+
     inline int string_to_int(const std::string &input) {
         int result;
         std::from_chars(input.data(), input.data() + input.size(), result);
         return result;
+    }
+
+    // inline std::vector<int> string_to_int(const std::vector<const char*> &input) {
+    //     return input | std::views::transform([](const char* ch){return std::string{ch};})
+    //     | std::ranges::to<std::vector>();
+    // }
+
+    inline std::vector<int> string_to_int(const std::vector<std::string> &input) {
+        return input | std::views::transform([](const std::string &str) {
+                   return std::stoi(str);
+               })
+               | std::ranges::to<std::vector>();
     }
 
     inline std::vector<std::string> load_to_vector(const std::filesystem::path &file_path) {
@@ -35,11 +68,11 @@ namespace util {
         return std::move(input_lines);
     }
 
-    inline std::vector<std::vector<std::string_view> > split_vector_of_strings(
+    inline std::vector<std::vector<std::string> > split_vector_of_strings(
         const std::vector<std::string> &input, const std::string_view delim) {
         return input | std::views::transform([delim](const std::string &str) {
             return std::views::split(str, delim)
-                   | std::views::transform([](const auto &a) { return std::string_view{a}; })
+                   | std::views::transform([](const auto &a) { return std::string(std::string_view{a}); })
                    | std::ranges::to<std::vector>();
         }) | std::ranges::to<std::vector>();
     }
